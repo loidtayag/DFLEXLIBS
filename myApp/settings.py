044777b66aa5d1c6fa8ddd5b03b439b1bac735ee
LoidTayag <loidtayag@gmail.com>
2024-03-16 20:19:15 +0000

    Take ?

--- myApp/settings.py
+++ myApp/settings.py
@@ -116,10 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-import os
 STATIC_URL = 'mySite/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mySite/static')],
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'mySite/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
