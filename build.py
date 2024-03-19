c41776bb51057bcebb3536993ad802038625ed1c
LoidTayag <loidtayag@gmail.com>
2024-03-19 20:42:10 +0000

    Added script to duplicates images and keep the structure

---
+++ build.py
@@ -0,0 +1,19 @@
+# Duplicates images from repository into the "mySite/static" 
+# while keeping the directory structure for deployment
+
+import os
+import shutil
+
+def getImages(srcDir, desDir):
+    for item in os.listdir(srcDir):
+        filePath = os.path.join(srcDir, item)
+        desPath = os.path.join(desDir, item)
+
+        if item.lower().endswith('.png'): 
+            if not os.path.exists(desDir):
+                os.makedirs(desDir) 
+            shutil.copy2(filePath, desDir)
+        elif os.path.isdir(filePath):  
+            getImages(filePath, desPath)
+
+getImages('mySite\DFLEXLIBS\controls', 'mySite/static/')
